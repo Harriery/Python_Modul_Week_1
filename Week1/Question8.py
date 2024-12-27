fe125490c2c55fc0692818405e1ea90643cd8aeb
okayzhr <okayzhr@gmail.com>
2024-12-27 12:14:28 +0100

    Delete Question8.py

--- Week1/Question8.py
+++
@@ -1,7 +0,0 @@
-#Question 8: Write a Python code that takes a word from the user and prints the reverse of this word on the screen.
-
-kelime=input("bir kelime yaziniz:")
-
-ters_kelime=kelime[::-1]
-
-print(ters_kelime)
